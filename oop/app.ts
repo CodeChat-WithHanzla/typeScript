d9b31123ae1c150ec2cddd50933d7b273450e59a
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2025-06-11 18:06:46 +0500

    oop ended here

---
+++ oop/app.ts
@@ -0,0 +1,74 @@
+// class User {
+//   constructor(
+//     public name: string,
+//     public age: number,
+//     public isAdmin: boolean = false,
+//     public study: string
+//   ) {}
+//   getUser() {
+//     console.log(this.name, this.age, this.isAdmin);
+//   }
+// }
+
+// let user = new User("Hanzla", 20, "BSCS"); it will cause a issue becoz of disorder ,  parameters with default values at the end of the constructor. It's a best practice and avoids these issues.
+// user.getUser();
+
+class User {
+  constructor(
+    public name: string,
+    public age: number,
+    public study: string,
+    public isAdmin: boolean = false
+  ) {}
+  getUser() {
+    return {
+      name: this.name,
+      age: this.age,
+      education: this.study,
+      isAdmin: this.isAdmin
+    };
+  }
+}
+let user = new User("Hanzla", 20, "BSCS");
+console.log(user.getUser());
+
+// Getters and Setters
+class Employee {
+  constructor(public _name:string){}
+  set name(name:string){
+    this._name = name
+  }
+  get name(){
+    return this._name
+  }
+}
+let emp = new Employee("Hanzla")
+emp.name = "Huzaifa"
+console.log(emp.name);
+
+// Static members of a class 
+class Calulator {
+  static PI = 3.14;
+  constructor(private radius:number){
+  }
+  calculate_area (){
+    return 2*Calulator.PI*this.radius
+  }
+}
+// Static members belongs to class itself not its instance 
+// So without creating the instance of the class , we can access PI 
+console.log(Calulator.PI);
+
+// Abstract class 
+// A class that cant be initaited , and act as a base class for others 
+abstract class Payment {
+  constructor(protected amount:number,protected account_number:number){
+    if(amount<0 || !amount)
+      throw new Error("Enter Valid Amount.")
+    if(!account_number)
+      throw new Error("Account number Required.")
+  }
+}
+class payPal extends Payment{
+  // payPal payment Integration here 
+}
